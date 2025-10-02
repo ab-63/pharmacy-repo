8ab013fd414e66d1b6e957160d9691c6ef31b7a8
Abdul Matin <ab@gmail.com>
2025-10-02 17:48:48 +0430

    File updated

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base:process.env.VITE_BASE-PATH || "",
-  server:{
-    port:3000
+  plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE - PATH || "pharmacy-repo",
+  server: {
+    port: 3000,
   },
   optimizeDeps: {
-    exclude: ['chartjs-adapter-date-fns']
-  }
-})
+    exclude: ["chartjs-adapter-date-fns"],
+  },
+});
 // vite.config.js
-
