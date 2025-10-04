939448102b7cc123438230149db01fe05830a5b3
Abdul Matin <ab@gmail.com>
2025-10-04 10:46:48 +0430

    Removed wrong bass path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/pharmacy-repo",
   server: {
     port: 3000,
   },
